7e8520310be0e338e256029d0ec6cbc9cb847500
acollier / dgriffis <acollier@glgroup.com , dgriffis@glgroup.com>
2014-03-12 16:53:04 -0400

    grunt stuff

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    coffee:{
+      dist: {
+        files: {
+          'malory.js' : ['malory.coffee']
+        },
+        options: {
+          bare: true,
+          sourceMap: false
+        }
+      }
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'nodeunit']
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('dev', ['coffee']);
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
+
+};
