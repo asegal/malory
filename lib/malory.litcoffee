b8796fc5644acd125099078850a1211039260033
acollier / dgriffis <acollier@glgroup.com , dgriffis@glgroup.com>
2014-03-17 09:24:03 -0400

    change to litcoffee - start docs

---
+++ lib/malory.litcoffee
@@ -0,0 +1,49 @@
+## malory 
+
+    malory = (config) ->
+      # Private
+      machinations = {}
+      workers = {}
+      budgetedWorkers = 50
+
+      sendMessage = (worker, message) ->
+        new Promise (resolve, reject) ->
+          listen = (e) ->
+            if (e.data.demand == message.demand)
+              e.currentTarget.removeEventListener("message", listen)
+              resolve e.data
+          worker.addEventListener("message", listen)
+          worker.postMessage(message)
+
+      initializeWorker = (configEntry) ->
+        worker = new Worker(configEntry.workerUrl)
+        workers[configEntry.name + '-' + configEntry.counter] = worker
+        message =
+          demand: configEntry.initialDemand
+          workerArguments: configEntry.workerArguments
+        sendMessage(worker, message).then (data) ->
+          if data[configEntry.officiallyOutOfMemory]
+            configEntry.counter++
+            configEntry.workerArguments = data.workerArguments
+            initializeWorker(configEntry) unless configEntry.counter > configEntry.budgetWorkers
+      
+      initialize = (config) ->
+        for configEntry, i in config
+          configEntry.name = i unless configEntry.name
+          configEntry.budgetedWorkers = budgetedWorkers unless configEntry.budgetedWorkers
+          configEntry.counter = 0
+          initializeWorker configEntry
+
+      # Send message to all workers, returns a promise, which will return an array containg each workers response as the index values
+      machinations.demand = (demand, workerArguments) ->
+        promiseArray = []
+        for key, worker of workers
+          message =
+            demand: demand
+            workerArguments: workerArguments
+          promiseArray.push sendMessage(worker,message)
+        Promise.all(promiseArray)
+      
+      initialize config
+
+      return machinations
---
+++ malory.js
@@ -0,0 +1,70 @@
+var malory;
+
+malory = function(config) {
+  var budgetedWorkers, initialize, initializeWorker, machinations, sendMessage, workers;
+  machinations = {};
+  workers = {};
+  budgetedWorkers = 50;
+  sendMessage = function(worker, message) {
+    return new Promise(function(resolve, reject) {
+      var listen;
+      listen = function(e) {
+        if (e.data.demand === message.demand) {
+          e.currentTarget.removeEventListener("message", listen);
+          return resolve(e.data);
+        }
+      };
+      worker.addEventListener("message", listen);
+      return worker.postMessage(message);
+    });
+  };
+  initializeWorker = function(configEntry) {
+    var message, worker;
+    worker = new Worker(configEntry.workerUrl);
+    workers[configEntry.name + '-' + configEntry.counter] = worker;
+    message = {
+      demand: configEntry.initialDemand,
+      workerArguments: configEntry.workerArguments
+    };
+    return sendMessage(worker, message).then(function(data) {
+      if (data[configEntry.officiallyOutOfMemory]) {
+        configEntry.counter++;
+        configEntry.workerArguments = data.workerArguments;
+        if (!(configEntry.counter > configEntry.budgetWorkers)) {
+          return initializeWorker(configEntry);
+        }
+      }
+    });
+  };
+  initialize = function(config) {
+    var configEntry, i, _i, _len, _results;
+    _results = [];
+    for (i = _i = 0, _len = config.length; _i < _len; i = ++_i) {
+      configEntry = config[i];
+      if (!configEntry.name) {
+        configEntry.name = i;
+      }
+      if (!configEntry.budgetedWorkers) {
+        configEntry.budgetedWorkers = budgetedWorkers;
+      }
+      configEntry.counter = 0;
+      _results.push(initializeWorker(configEntry));
+    }
+    return _results;
+  };
+  machinations.demand = function(demand, workerArguments) {
+    var key, message, promiseArray, worker;
+    promiseArray = [];
+    for (key in workers) {
+      worker = workers[key];
+      message = {
+        demand: demand,
+        workerArguments: workerArguments
+      };
+      promiseArray.push(sendMessage(worker, message));
+    }
+    return Promise.all(promiseArray);
+  };
+  initialize(config);
+  return machinations;
+};
